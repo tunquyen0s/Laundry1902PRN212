985ce946ebbbc4144fafff1b4dd17fce716c2fff
TriDepTrai <trilt.ce191249@gmail.com>
2025-11-03 03:29:32 +0700

    Manage Resources
    Thêm:
    Xem được view của quản lí tài nguyên
    Có thể dùng CRUD
    Có thể search theo tên
    Có thể clear tất cả trong input
    Có thể refresher trang
    
    Lỗi: Chưa thể validation quantity, price per unit.
    Thông báo tài nguyên nào dưới 1 số lượng nhất định chưa forrmat cho nó như là Kg thì phải dưới bao nhiêu Lít thì phải dưới bao nhiêu.

---
+++ LaundryWPF/Views/ManageResources.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LaundryWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LaundryWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for ManageResources.xaml
+    /// </summary>
+    public partial class ManageResources : Page
+    {
+        public ManageResources()
+        {
+            InitializeComponent();
+            DataContext = new ManageResourcesViewModel();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
